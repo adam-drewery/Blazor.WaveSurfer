e98c9528066728224dad2569a05a3eb4488a40a2
Adam Drewery <8437373+adam-drewery@users.noreply.github.com>
2023-08-25 16:49:57 +0200

    stuck with this because when calling a method, we don't know the return type yet, so using ISObjectReference doesn't work when the return type is a primitive such as bool. Maybe we could try returning a model type with a JSRuntimeObject property but i don't think the interop engine handles that.

--- Events.cs
+++ Events.cs
@@ -1,62 +1,22 @@
+using Blazor.InterOptimal;
 using Microsoft.JSInterop;
 
 namespace Blazor.WaveSurfer;
 
 internal class Events
 {
-    private static bool _configured;
-    private readonly IJSRuntime _jsRuntime;
-    private readonly IJSObjectReference _jsObject;
+    private readonly dynamic _scriptObject;
 
-    private Events(IJSRuntime jsRuntime, IJSObjectReference jsObject)
+    private Events(ScriptObject scriptObject) => _scriptObject = scriptObject;
+
+    public static Events Setup(ScriptObject scriptObject)
     {
-        _jsRuntime = jsRuntime;
-        _jsObject = jsObject;
+        return new Events(scriptObject);
     }
 
-    public static async Task<Events> Setup(IJSRuntime jsRuntime, IJSObjectReference jsObject)
+    public async Task WireUp(WaveSurfer receiver, string eventName)
     {
-        if (!_configured)
-            await Configure(jsRuntime);
-        
-        _configured = true;
-        
-        return new Events(jsRuntime, jsObject);
-    }
-
-    private static async Task Configure(IJSRuntime jsRuntime)
-    {
-        // setup utility js function first
-        const string setupCallback = 
-            @"window.setupCallback = function(eventReceiver, eventEmitter, eventName) {
-            eventEmitter.on(eventName, function(args) {
-                eventReceiver.invokeMethodAsync('OnEvent', eventName, args);
-            });
-        };";
-
-        const string blobWrapper =
-            @"window.blobWrapper = function(instance, methodName, ...args) {
-            const processedArgs = args.map(arg => {
-                if (arg instanceof Uint8Array) {
-                    return new Blob([arg]);
-                }
-                return arg;
-            });
-
-            if (instance && typeof instance[methodName] === 'function') {
-                instance[methodName](...processedArgs);
-            } else {
-                console.error(`Method ${methodName} is not defined on the provided instance.`);
-            }
-        };";
-
-        await jsRuntime.InvokeVoidAsync("eval", setupCallback);
-        await jsRuntime.InvokeVoidAsync("eval", blobWrapper);
-    }
-
-    public async Task WireUp(object receiver, string eventName)
-    {
-        var @ref = DotNetObjectReference.Create(receiver);
-        await _jsRuntime.InvokeVoidAsync("setupCallback", @ref, _jsObject, eventName);
+        var func = (dynamic args) => receiver.OnEvent(eventName, args);
+        await _scriptObject.on(eventName, func);
     }
 }
